1f822ac5d16b3910fdf458f5eca4038022ae222a
Mary M. Zysk <doumkatek42@gmail.com>
2019-04-14 18:48:08 -0400

    new additions

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: 'app/app.component.html',
   styleUrls: ['app/app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  onMediaItemDelete(mediaItem) {}
+  firstMediaItem = {
+    id: 1,
+    name: "Firebug",
+    medium: "Series",
+    category: "Science Fiction",
+    year: 2010,
+    //watchedOn: 1294166565384,
+    watchedOn:null,
+    isFavorite: false
+  };
+}
--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: 'app/media-item.component.html',
   styleUrls: ['app/media-item.component.css']
 })
-export class MediaItemComponent { }
+export class MediaItemComponent {
+  //'mediaItemToWatch is an alias for mediaItem, mostly don't want to use it
+  @Input('mediaItem') mediaItem;
+  //exposing an event that can be subscribed to like a native dom emit
+  @Output() delete = new EventEmitter();
+
+  onDelete() {
+    alert('deleting');
+    
+    //passes the media item to the emit
+    this.delete.emit(this.mediaItem);
+  }
+
+ }
